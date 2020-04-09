6029dbb05c12f90f3a9fb543c3ed0a3e20870f9d
Renat <renat.blinov@yandex.ru>
2020-04-09 21:58:47 +0300

    Change to RestController

--- src/main/java/ru/itis/context/controllers/UserPageController.java
+++ src/main/java/ru/itis/context/controllers/UserPageController.java
@@ -1,26 +1,35 @@
 package ru.itis.context.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.itis.context.models.User;
-import ru.itis.context.security.details.UserDetailsImpl;
+import ru.itis.context.security.annotations.AuthenticatedUser;
+import ru.itis.context.security.jwt.details.UserDetailsImpl;
 import ru.itis.context.services.UsersService;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserPageController {
 
     @Autowired
     private UsersService service;
 
-    @GetMapping("/user")
-    public String getUserPage(Authentication authentication, Model model) {
-        UserDetails userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        User user = service.getUserByUsername(userDetails.getUsername());
-        model.addAttribute("user", user);
-        return "userPage";
+    @GetMapping
+    public ResponseEntity<User> getUserPage(@AuthenticatedUser UserDetailsImpl userDetails1,
+                                            @AuthenticationPrincipal UserDetailsImpl userDetails2) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getDetails();
+
+        System.out.println(userDetails1.toString());
+        System.out.println(userDetails2.toString());
+        
+        return ResponseEntity.ok(service.getUserByUsername(userDetails.getUsername()));
     }
 }
